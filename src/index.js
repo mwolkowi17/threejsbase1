6421064aaef54db46729cd5ccc706fd0547f4279
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-05-16 20:38:12 +0200

    small changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { Scene } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,8 +10,20 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const materialB = new THREE.MeshBasicMaterial({ color: 0x0000FFFF });
 const cube = new THREE.Mesh(geometry, material);
+
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+//points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometryB = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometryB, materialB );
+cube.add( line );
 scene.add(cube);
+
 
 camera.position.z = 5;
 
