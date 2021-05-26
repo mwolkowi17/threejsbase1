b6b1b656341cd6b36fb8c2d857f86928e102ee86
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-05-26 09:24:41 +0200

    new rotation speed

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ camera.position.z = 5;
 const animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
+    cube.rotation.x += 0.02;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
