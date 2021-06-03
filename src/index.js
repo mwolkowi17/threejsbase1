9dd8c9133d12142a98e543ca41b836b8de52716e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-03 20:08:16 +0200

    new light added

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ const skyColor = 0xB1E1FF;  // light blue
 const groundColor = 0xB97A20;  // brownish orange
 const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
 //light.position.set( 50, 50, 50 );
+const color1 = 0xFFFFFF;
+const intensity1 = 0.2;
+const light2 = new THREE.DirectionalLight(color1, intensity1);
+light2.position.set(-10, 10, 0);
+light2.target.position.set(-5, 0, 0);
 
 const points = [];
 points.push( new THREE.Vector3( - 10, 0, 0 ) );
@@ -29,7 +34,8 @@ const line = new THREE.Line( geometryB, materialB );
 cube.add( line );
 scene.add(cube);
 scene.add( light );
-
+scene.add(light2);
+scene.add(light2.target);
 
 camera.position.z = 5;
 
