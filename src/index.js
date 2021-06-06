efaa1e83e7e2c6b838689aba9767b9e101192ef8
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-06 09:59:23 +0200

    position js added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const btn = document.getElementById("action");
 
 btn.addEventListener("click", function () {
     //Do something here
-    let boxwidthinput = document.getElementById("myInput").value
+    let boxwidthinput = document.getElementById("myInput").value;
     let boxwidthinputnumber = parseFloat(boxwidthinput);
     if (boxwidthinputnumber != 0) {
         cube.scale.x = boxwidthinputnumber;
@@ -14,7 +14,17 @@ btn.addEventListener("click", function () {
 
     //cube.position.x=2;
 });
+// position segment
 
+const btnposition = document.getElementById("actionPosition");
+
+btnposition.addEventListener("click", function(){
+    let boxpositionxinput = document.getElementById("positionInput").value;
+    let boxpositionxinputnumber = parseFloat(boxpositionxinput);
+    if (boxpositionxinputnumber != 0) {
+        cube.position.x = boxpositionxinputnumber;
+    }
+})
 //cube.position.x=2; do zrobienia
 
 
