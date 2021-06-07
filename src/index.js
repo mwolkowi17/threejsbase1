7ae368026ba40301d55acb7205ed98c25b26c0cc
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-07 12:04:16 +0200

    small refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { Scene } from 'three';
+import {cube} from './cube.js'
 
 // input segment
 const btn = document.getElementById("action");
@@ -37,15 +38,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
-const width = 2;
-const height = 1;
-const depth = 1;
-const geometry = new THREE.BoxGeometry(width, height, depth);
-const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-//onst material = new THREE.MeshPhongMaterial();
 const materialB = new THREE.MeshBasicMaterial({ color: 0x0000FFFF });
-const cube = new THREE.Mesh(geometry, material);
 const intensity = 1;
 const skyColor = 0xB1E1FF;  // light blue
 const groundColor = 0xB97A20;  // brownish orange
